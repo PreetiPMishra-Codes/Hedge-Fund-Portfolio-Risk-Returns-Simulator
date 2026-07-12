fde6db3d147f8366b647fcb24ba61e9e48459e48
Preeti Pragyan Mishra <preetipmishra2@gmail.com>
2026-07-12 11:57:42 +0530

    debugged

--- src/historical_variance.py
+++ src/historical_variance.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 #historical VAR
-def caclculate_historical _var(portfolio_ret:pd.Series,confidence_level:float=0.95)->float:
+def calculate_historical_var(portfolio_ret:pd.Series,confidence_level:float=0.95)->float:
     """
     Historical VaR: looks at the ACTUAL distribution of past returns
     and finds the cutoff below which only (1 - confidence_level)% of
